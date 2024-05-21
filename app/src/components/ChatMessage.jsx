c2550a1d4aa38ad802b7ab8f41262433b6a4fe43
andereyaa <andrewmamawi@gmail.com>
2024-05-21 20:59:40 +0300

    removed vite scaffolding

--- app/src/components/ChatMessage.jsx
+++ app/src/components/ChatMessage.jsx
@@ -3,3 +3,15 @@ export default function ChatMessage(props){
 
     return(<></>)
 }
+
+
+
+/**
+ * 
+ * git fetch --all
+ * git pull
+ * git branch --remote
+ * git checkout implement-basic-structure
+ * git pull
+ * 
+ */
